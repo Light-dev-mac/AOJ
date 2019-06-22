5851f056cb897ef7de437c710309b9fe7203b6c1
yuhori <berliner.philharmoniker.karajan@gmail.com>
2019-06-22 19:56:59 +0900

    Watch

--- C/ITP1_1_D.c
+++ C/ITP1_1_D.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int num;
+    int num; 
     scanf("%d", &num);
     printf("%d:%d:%d\n", num/3600, (num%3600)/60,num%60);
     return 0;
--- Python/ITP1_1_D.py
+++ Python/ITP1_1_D.py
@@ -1,2 +1,2 @@
 num = int(input())
-print('{}:{}:{}'.format(num//3600, (num%3600)//60, num%60))
+print('{}:{}:{}'.format(num//3600, (num%3600)//60, num%60)) 
