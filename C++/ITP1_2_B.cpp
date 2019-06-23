c302ce2e031d8cf9e4d6856f0e0fb5daebf1c65f
yuhori <berliner.philharmoniker.karajan@gmail.com>
2019-06-23 12:13:46 +0900

    Range

--- C++/ITP1_2_B.cpp
+++ C++/ITP1_2_B.cpp
@@ -1,11 +1,15 @@
-#include<iostream>
-int main(){
-    using namespace std;
-    int a,b,c;
-    cin>>a>>b>>c;
-    if(a<b&&b<c)
-        cout<<"Yes"<<endl;
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a >= 0 && a < b & b < c && c <= 100)
+    {
+        cout << "Yes" << endl;
+    }
     else
-        cout<<"No"<<endl;
-
-}
+    {
+        cout << "No" << endl;
+    }
+}1
