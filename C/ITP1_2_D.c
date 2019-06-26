5c8a49b2e954d8364d812e945d5000ce16cc3d77
yuhori <berliner.philharmoniker.karajan@gmail.com>
2019-06-27 05:33:32 +0900

    Circle in a Rectangle

---
+++ C/ITP1_2_D.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int W, H, x, y, r;
+    scanf("%d %d %d %d %d", &W, &H, &x, &y, &r);
+    if (x >= r && x + r <= W && y >= r && y + r <= H)
+        printf("Yes\n");
+    else
+        printf("No\n");
+    return 0;
+}
