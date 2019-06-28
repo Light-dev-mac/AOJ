b1f0c1dbb39681b2e3affcb53b0c5ccac25f9206
yuhori <berliner.philharmoniker.karajan@gmail.com>
2019-06-28 20:39:04 +0900

    Print Many Hello World

--- C++/ITP1_3_A.cpp
+++ C++/ITP1_3_A.cpp
@@ -1,8 +1,9 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-int main(){
-    for(int i=0;i<1000;i++){
-        cout << "Hello World"<<endl;
+int main()
+{
+    for (int i = 0; i < 1000; i++)
+    {
+        cout << "Hello World" << endl;
     }
 }
