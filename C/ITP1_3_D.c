89cc9ede3918410eabecccaa6db6fd50f813630e
yuhori <berliner.philharmoniker.karajan@gmail.com>
2019-07-07 14:40:58 +0900

    How Many Divisors?

--- C/ITP1_3_D.c
+++ C/ITP1_3_D.c
@@ -1,14 +1,14 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 int main()
 {
     int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
     int num = 0;
-    cin >> a >> b >> c;
     for (int i = a; i <= b; i++)
     {
         if (c % i == 0)
             num++;
     }
-    cout << num << endl;
+    printf("%d\n", num);
+    return 0;
 }
